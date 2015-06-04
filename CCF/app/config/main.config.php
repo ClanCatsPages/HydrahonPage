aefdd0d9993100ff8a94273c93a41aa53dcae62b
Mario Döring <deluna.yy@gmail.com>
2015-06-04 22:53:39 +0200

    ClanCatsFramework int

---
+++ CCF/app/config/main.config.php
@@ -0,0 +1,27 @@
+<?php 
+/*
+ *---------------------------------------------------------------
+ * Main CCF configuration
+ *---------------------------------------------------------------
+ *
+ * Take a look at the core main configuration file to see 
+ * what options are around. .../CCF/vendor/clancats/core/config/main.config.php
+ */
+return array(
+	
+	/*
+	 * URL configuration
+	 */
+	'url'	=> array(
+		// if not in the root directory set the path offset.
+		'path'		=> '/',
+	),
+	
+	/*
+	 * Security
+	 */
+	'security' => array(
+		// it is really important that you choose your own one!
+		'salt' => 'HdD21Ut/}Ã&:Ã„hP¶\ZB{œN}9{O?+$,+',
+	),
+);
---
+++ README.md
@@ -0,0 +1,3 @@
+# HydrahonPage
+
+The hydrahon website.
